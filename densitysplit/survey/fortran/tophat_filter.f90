64637917b6efb3aa848455b722ad728ab032353b
epaillas <epaillas@astro.puc.cl>
2021-04-17 16:36:50 -0400

    Add full LS estimator

--- densitysplit/survey/fortran/tophat_filter.f90
+++ densitysplit/survey/fortran/tophat_filter.f90
@@ -49,45 +49,49 @@ program tophat_filter
   implicit none
   
   real*8 :: rgrid, rfilter, gridmax, gridmin
-  real*8 :: disx, disy, disz, dis2, norm
-  real*8 :: dmin, dmax, dmin2, dmax2
+  real*8 :: disx, disy, disz, dis2
+  real*8 :: dim1_min, dim1_max, dim1_min2, dim1_max2
   
-  integer*8 :: ng, nc, nr
+  integer*8 :: ng1, ng2, nr1, nr2
   integer*8 :: i, ii, ix, iy, iz
   integer*8 :: nrows, ncols
   integer*8 :: ipx, ipy, ipz, ndif
   integer*8 :: ngrid
   integer*4 :: nthreads
   integer*8 :: end, beginning, rate
-  integer*8, dimension(:, :, :), allocatable :: lirst_tracers, lirst_randoms
+  integer*8, dimension(:, :, :), allocatable :: lirst_data2, lirst_random2
 
-  integer*8, dimension(:), allocatable :: ll_tracers, ll_randoms
+  integer*8, dimension(:), allocatable :: ll_data2, ll_random2
   
-  real*8, allocatable, dimension(:,:)  :: tracers, randoms, centres
-  real*8, dimension(:), allocatable :: DD, DR, delta
-  real*8, dimension(:), allocatable :: weights_tracers, weights_centres, weights_randoms
+  real*8, allocatable, dimension(:,:)  :: data1, data2, random1, random2
+  real*8, dimension(:), allocatable :: D1D2, D1R2, R1D2, R1R2, xi_r
+  real*8, dimension(:), allocatable :: weight_data1, weight_data2
+  real*8, dimension(:), allocatable :: weight_random1, weight_random2
 
   logical :: debug = .true.
   
   character(20), external :: str
-  character(len=500) :: input_tracers, input_centres, input_randoms, output_filter
+  character(len=500) :: data_filename2, data_filename1, output_filename
+  character(len=500) :: random_filename1, random_filename2
   character(len=10) :: dmax_char, dmin_char, gridmin_char, gridmax_char
   character(len=10) :: ngrid_char, rfilter_char, nthreads_char
-  character(len=10) :: estimator = 'DP'
+  character(len=2) :: estimator
 
-  if (iargc() .ne. 11) then
+  if (iargc() .ne. 13) then
     write(*,*) 'Some arguments are missing.'
-    write(*,*) '1) input_tracers'
-    write(*,*) '2) input_centres'
-    write(*,*) '3) input_randoms'
-    write(*,*) '4) output_filter'
-    write(*,*) '5) dmin'
-    write(*,*) '6) dmax'
-    write(*,*) '7) rfilter'
-    write(*,*) '8) ngrid'
-    write(*,*) '9) gridmin'
-    write(*,*) '10) gridmax'
-    write(*,*) '11) nthreads'
+    write(*,*) '1) data_filename1'
+    write(*,*) '2) data_filename2'
+    write(*,*) '3) random_filename1'
+    write(*,*) '4) random_filename2'
+    write(*,*) '5) output_filename'
+    write(*,*) '6) dim1_min'
+    write(*,*) '7) dim1_max'
+    write(*,*) '8) rfilter'
+    write(*,*) '9) ngrid'
+    write(*,*) '10) gridmin'
+    write(*,*) '11) gridmax'
+    write(*,*) '12) estimator'
+    write(*,*) '13) nthreads'
     write(*,*) ''
     stop
   end if
@@ -95,21 +99,23 @@ program tophat_filter
   call system_clock(beginning, rate)
 
   ! read arguments from command line
-  call getarg(1, input_tracers)
-  call getarg(2, input_centres)
-  call getarg(3, input_randoms)
-  call getarg(4, output_filter)
-  call getarg(5, dmin_char)
-  call getarg(6, dmax_char)
-  call getarg(7, rfilter_char)
-  call getarg(8, ngrid_char)
-  call getarg(9, gridmin_char)
-  call getarg(10, gridmax_char)
-  call getarg(11, nthreads_char)
+  call getarg(1, data_filename1)
+  call getarg(2, data_filename2)
+  call getarg(3, random_filename1)
+  call getarg(4, random_filename2)
+  call getarg(5, output_filename)
+  call getarg(6, dmin_char)
+  call getarg(7, dmax_char)
+  call getarg(8, rfilter_char)
+  call getarg(9, ngrid_char)
+  call getarg(10, gridmin_char)
+  call getarg(11, gridmax_char)
+  call getarg(12, estimator)
+  call getarg(13, nthreads_char)
   
   ! convert string arguments to corresponding data types
-  read(dmin_char, *) dmin
-  read(dmax_char, *) dmax
+  read(dmin_char, *) dim1_min
+  read(dmax_char, *) dim1_max
   read(rfilter_char, *) rfilter
   read(ngrid_char, *) ngrid
   read(gridmin_char, *) gridmin
@@ -122,110 +128,142 @@ program tophat_filter
     write(*,*) 'Running tophat_filter.exe'
     write(*,*) 'input parameters:'
     write(*,*) ''
-    write(*, *) 'input_tracers: ', trim(input_tracers)
-    write(*, *) 'input_centres: ', trim(input_centres)
-    write(*, *) 'output_filter: ', trim(output_filter)
-    write(*, *) 'dmin: ', trim(dmin_char), ' Mpc'
-    write(*, *) 'dmax: ', trim(dmax_char), ' Mpc'
-    write(*, *) 'rfilter: ', trim(rfilter_char), 'Mpc'
-    write(*, *) 'ngrid: ', trim(ngrid_char)
-    write(*, *) 'gridmin: ', trim(gridmin_char), 'Mpc'
-    write(*, *) 'gridmax: ', trim(gridmax_char), 'Mpc'
-    write(*, *) 'nthreads: ', trim(nthreads_char)
+    write(*,*) 'data_filename1: ', trim(data_filename1)
+    write(*,*) 'data_filename2: ', trim(data_filename2)
+    write(*,*) 'random_filename1: ', trim(random_filename1)
+    write(*,*) 'random_filename2: ', trim(random_filename2)
+    write(*,*) 'output_filename: ', trim(output_filename)
+    write(*,*) 'dmin: ', trim(dmin_char), ' Mpc'
+    write(*,*) 'dmax: ', trim(dmax_char), ' Mpc'
+    write(*,*) 'rfilter: ', trim(rfilter_char), 'Mpc'
+    write(*,*) 'ngrid: ', trim(ngrid_char)
+    write(*,*) 'gridmin: ', trim(gridmin_char), 'Mpc'
+    write(*,*) 'gridmax: ', trim(gridmax_char), 'Mpc'
+    write(*,*) 'estimator: ', trim(estimator)
+    write(*,*) 'nthreads: ', trim(nthreads_char)
     write(*,*) ''
   end if
 
-  ! read tracers catalogue
-  open(10, file=input_tracers, status='old', form='unformatted')
+  
+  ! read data catalogue #1
+  open(11, file=data_filename1, status='old', form='unformatted')
+  read(11) nrows
+  read(11) ncols
+  allocate(data1(ncols, nrows))
+  allocate(weight_data1(nrows))
+  read(11) data1
+  close(11)
+  ng1 = nrows
+  if (ncols .eq. 4) then
+    weight_data1 = data1(4, :)
+    if (debug) write(*,*) 'Data file 1 has weight information.'
+  else
+    weight_data1 = 1.0
+  end if
+  if (debug) then
+    write(*,*) 'ndata1 dim: ', size(data1, dim=1), size(data1, dim=2)
+    write(*,*) 'data1(min), data2(max) = ', minval(data1(:,:)), maxval(data1(:,:))
+  end if
+
+  ! read data catalogue #2
+  open(10, file=data_filename2, status='old', form='unformatted')
   read(10) nrows
   read(10) ncols
-  allocate(tracers(ncols, nrows))
-  allocate(weights_tracers(nrows))
-  read(10) tracers
+  allocate(data2(ncols, nrows))
+  allocate(weight_data2(nrows))
+  read(10) data2
   close(10)
-  ng = nrows
+  ng2 = nrows
   if (ncols .eq. 4) then
-    weights_tracers = tracers(4, :)
-    if (debug) write(*,*) 'Tracer file has weight information.'
+    weight_data2 = data2(4, :)
+    if (debug) write(*,*) 'Data file 2 has weight information.'
   else
-    weights_tracers = 1.0
+    weight_data2 = 1.0
   end if
   if (debug) then
-    write(*,*) 'ntracers dim: ', size(tracers, dim=1), size(tracers, dim=2)
-    write(*,*) 'tracers(min), tracers(max) = ', minval(tracers(:,:)), maxval(tracers(:,:))
+    write(*,*) 'ndata2 dim: ', size(data2, dim=1), size(data2, dim=2)
+    write(*,*) 'data2(min), data2(max) = ', minval(data2(:,:)), maxval(data2(:,:))
   end if
 
-  ! read centres catalogue
-  open(11, file=input_centres, status='old', form='unformatted')
-  read(11) nrows
-  read(11) ncols
-  allocate(centres(ncols, nrows))
-  allocate(weights_centres(nrows))
-  read(11) centres
-  close(11)
-  nc = nrows
-  if (ncols .eq. 4) then
-    weights_centres = centres(4, :)
-    if (debug) write(*,*) 'Centres file has weight information.'
-  else
-    weights_centres = 1.0
-  end if
-  if (debug) then
-    write(*,*) 'ncentres dim: ', size(centres, dim=1), size(centres, dim=2)
-    write(*,*) 'centres(min), tracers(max) = ', minval(centres(:,:)), maxval(centres(:,:))
+  if (estimator .eq. 'LS') then
+    ! read random catalogue #1
+    open(11, file=random_filename1, status='old', form='unformatted')
+    read(11) nrows
+    read(11) ncols
+    allocate(random1(ncols, nrows))
+    allocate(weight_random1(nrows))
+    read(11) random1
+    close(11)
+    nr1 = nrows
+    if (ncols .eq. 4) then
+      weight_random1 = random1(4, :)
+      if (debug) write(*,*) 'Random file 1 has weight information.'
+    else
+      weight_random1 = 1.0
+    end if
+    if (debug) then 
+      write(*,*) 'nrandom2 dim: ', size(random1, dim=1), size(random1, dim=2)
+      write(*,*) 'random2(min), random2(max) = ', minval(random1(:,:)), maxval(random1(:,:))
+    end if
   end if
 
-  ! read random catalogue
-  open(11, file=input_randoms, status='old', form='unformatted')
+  ! read random2 catalogue
+  open(11, file=random_filename2, status='old', form='unformatted')
   read(11) nrows
   read(11) ncols
-  allocate(randoms(ncols, nrows))
-  allocate(weights_randoms(nrows))
-  read(11) randoms
+  allocate(random2(ncols, nrows))
+  allocate(weight_random2(nrows))
+  read(11) random2
   close(11)
-  nr = nrows
+  nr2 = nrows
   if (ncols .eq. 4) then
-    weights_randoms = randoms(4, :)
-    if (debug) write(*,*) 'Tracer file has weight information.'
+    weight_random2 = random2(4, :)
+    if (debug) write(*,*) 'Random file 2 has weight information.'
   else
-    weights_randoms = 1.0
+    weight_random2 = 1.0
   end if
   if (debug) then 
-    write(*,*) 'nrandoms dim: ', size(randoms, dim=1), size(randoms, dim=2)
-    write(*,*) 'randoms(min), randoms(max) = ', minval(randoms(:,:)), maxval(randoms(:,:))
+    write(*,*) 'nrandom2 dim: ', size(random2, dim=1), size(random2, dim=2)
+    write(*,*) 'random2(min), random2(max) = ', minval(random2(:,:)), maxval(random2(:,:))
   end if
   
-  ! construct linked lists for tracers and randoms
-  allocate(ll_tracers(ng))
-  allocate(ll_randoms(nr))
-  allocate(lirst_tracers(ngrid, ngrid, ngrid))
-  allocate(lirst_randoms(ngrid, ngrid, ngrid))
-  call linked_list(tracers, ngrid, gridmin, gridmax, ll_tracers, lirst_tracers, rgrid)
-  call linked_list(randoms, ngrid, gridmin, gridmax, ll_randoms, lirst_randoms, rgrid)
+  ! construct linked lists for data2 and random2
+  allocate(ll_data2(ng2))
+  allocate(ll_random2(nr2))
+  allocate(lirst_data2(ngrid, ngrid, ngrid))
+  allocate(lirst_random2(ngrid, ngrid, ngrid))
+  call linked_list(data2, ngrid, gridmin, gridmax, ll_data2, lirst_data2, rgrid)
+  call linked_list(random2, ngrid, gridmin, gridmax, ll_random2, lirst_random2, rgrid)
 
   ! calculate number counts around each centre
-  allocate(DD(nc))
-  allocate(DR(nc))
-  allocate(delta(nc))
-  DD = 0
-  DR = 0
-  delta = 0
-  ndif = int(dmax / rgrid + 1.)
-  dmin2 = dmin ** 2
-  dmax2 = dmax ** 2
+  allocate(D1D2(ng1))
+  allocate(D1R2(ng1))
+  allocate(xi_r(ng1))
+  D1D2 = 0
+  D1R2 = 0
+  if (estimator .eq. 'LS') then
+    allocate(R1R2(nr1))
+    allocate(R1D2(nr1))
+    R1R2 = 0
+    R1D2 = 0
+  end if
+  
+  ndif = int(dim1_max / rgrid + 1.)
+  dim1_min2 = dim1_min ** 2
+  dim1_max2 = dim1_max ** 2
   
 
   call OMP_SET_NUM_THREADS(nthreads)
   if (debug) then
-    write(*, *) 'Maximum number of threads: ', OMP_GET_MAX_THREADS()
+    write(*,*) 'Maximum number of threads: ', OMP_GET_MAX_THREADS()
   end if
 
   !$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(i, ipx, ipy, ipz, &
   !$OMP& ix, iy, iz, ii, disx, disy, disz, dis2)
-  do i = 1, nc
-    ipx = int((centres(1, i) - gridmin) / rgrid + 1.)
-    ipy = int((centres(2, i) - gridmin) / rgrid + 1.)
-    ipz = int((centres(3, i) - gridmin) / rgrid + 1.)
+  do i = 1, ng1
+    ipx = int((data1(1, i) - gridmin) / rgrid + 1.)
+    ipy = int((data1(2, i) - gridmin) / rgrid + 1.)
+    ipz = int((data1(3, i) - gridmin) / rgrid + 1.)
 
     ! loop over cells around each centre
     do ix = ipx - ndif, ipx + ndif, 1
@@ -233,42 +271,42 @@ program tophat_filter
         do iz = ipz - ndif, ipz + ndif, 1
           if ((ix-ipx)**2 + (iy-ipy)**2 + (iz-ipz)**2 .gt. (ndif + 1)**2) cycle
 
-          ! loop over tracers in each cell
-          ii = lirst_tracers(ix, iy, iz)
+          ! loop over data2 in each cell
+          ii = lirst_data2(ix, iy, iz)
           if (ii .ne. 0) then
             do
-              ii = ll_tracers(ii)
-              disx = tracers(1, ii) - centres(1, i)
-              disy = tracers(2, ii) - centres(2, i)
-              disz = tracers(3, ii) - centres(3, i)
+              ii = ll_data2(ii)
+              disx = data2(1, ii) - data1(1, i)
+              disy = data2(2, ii) - data1(2, i)
+              disz = data2(3, ii) - data1(3, i)
 
               dis2 = disx * disx + disy * disy + disz * disz
 
-              if (dis2 .gt. dmin2 .and. dis2 .lt. dmax2) then
-                DD(i) = DD(i) + weights_centres(i) * weights_tracers(ii)
+              if (dis2 .gt. dim1_min2 .and. dis2 .lt. dim1_max2) then
+                D1D2(i) = D1D2(i) + weight_data1(i) * weight_data2(ii)
               end if
   
-              if(ii.eq.lirst_tracers(ix, iy, iz)) exit
+              if(ii.eq.lirst_data2(ix, iy, iz)) exit
   
             end do
           end if
 
-          ! loop over randoms in each cell
-          ii = lirst_randoms(ix, iy, iz)
+          ! loop over random2 in each cell
+          ii = lirst_random2(ix, iy, iz)
           if (ii .ne. 0) then
             do
-              ii = ll_randoms(ii)
-              disx = randoms(1, ii) - centres(1, i)
-              disy = randoms(2, ii) - centres(2, i)
-              disz = randoms(3, ii) - centres(3, i)
+              ii = ll_random2(ii)
+              disx = random2(1, ii) - data1(1, i)
+              disy = random2(2, ii) - data1(2, i)
+              disz = random2(3, ii) - data1(3, i)
  
               dis2 = disx * disx + disy * disy + disz * disz
 
-              if (dis2 .gt. dmin2 .and. dis2 .lt. dmax2) then
-                DR(i) = DR(i) + weights_centres(i) * weights_randoms(ii)
+              if (dis2 .gt. dim1_min2 .and. dis2 .lt. dim1_max2) then
+                D1R2(i) = D1R2(i) + weight_data1(i) * weight_random2(ii)
               end if
   
-              if(ii .eq. lirst_randoms(ix, iy, iz)) exit
+              if(ii .eq. lirst_random2(ix, iy, iz)) exit
   
             end do
           end if
@@ -278,20 +316,86 @@ program tophat_filter
   end do
   !$OMP END PARALLEL DO
 
-  norm = SUM(weights_randoms) / SUM(weights_tracers)
+  if (estimator .eq. 'LS') then 
+    ! Loop over randoms # 1
+    !$OMP PARALLEL DO DEFAULT(SHARED) PRIVATE(i, ii, ipx, ipy, ipz, &
+    !$OMP ix, iy, iz, disx, disy, disz, dis2)
+    do i = 1, nr1
+      ipx = int((random1(1, i) - gridmin) / rgrid + 1.)
+      ipy = int((random1(2, i) - gridmin) / rgrid + 1.)
+      ipz = int((random1(3, i) - gridmin) / rgrid + 1.)
+
+      do ix = ipx - ndif, ipx + ndif, 1
+        do iy = ipy - ndif, ipy + ndif, 1
+          do iz = ipz - ndif, ipz + ndif, 1 
+            if ((ix - ipx)**2 + (iy - ipy)**2 + (iz - ipz)**2 .gt. (ndif + 1)**2) cycle
+
+            ii = lirst_random2(ix, iy, iz)
+            if (ii .ne. 0) then
+              do
+                ii = ll_random2(ii)
+                disx = random2(1, ii) - random1(1, i)
+                disy = random2(2, ii) - random1(2, i)
+                disz = random2(3, ii) - random1(3, i)
+
+                dis2 = disx * disx + disy * disy + disz * disz
+
+                if (dis2 .gt. dim1_min2 .and. dis2 .lt. dim1_max2) then
+                  R1R2(i) = R1R2(i) + weight_random1(i) * weight_random2(ii)
+                end if
+
+                  if(ii .eq. lirst_random2(ix, iy, iz)) exit
+
+              end do
+            end if
+
+            ii = lirst_data2(ix, iy, iz)
+            if (ii .ne. 0) then
+              do
+                ii = ll_data2(ii)
+                disx = data2(1, ii) - random1(1, i)
+                disy = data2(2, ii) - random1(2, i)
+                disz = data2(3, ii) - random1(3, i)
+
+                dis2 = disx * disx + disy * disy + disz * disz
+
+                if (dis2 .gt. dim1_min2 .and. dis2 .lt. dim1_max2) then
+                  R1D2(i) = R1D2(i) + weight_random1(i) * weight_data2(ii)
+                end if
+
+                  if(ii .eq. lirst_data2(ix, iy, iz)) exit
+
+              end do
+            end if
+          end do
+        end do
+      end do
+    end do
+    !$OMP END PARALLEL DO
+  end if
+
+  ! Normalize pair counts
+  D1D2 = D1D2 * 1. / (SUM(weight_data1) * SUM(weight_data2))
+  D1R2 = D1R2 * 1. / (SUM(weight_data1) * SUM(weight_random2))
+  if (estimator .eq. 'LS') then
+    R1R2 = R1R2 * 1. / (SUM(weight_random1) * SUM(weight_random2))
+    R1D2 = R1D2 * 1. / (SUM(weight_random1) * SUM(weight_data2))
+  end if
 
   ! Calculate density contrast
   if (estimator .eq. 'DP') then
-    delta = norm * (DD / DR) - 1
+    xi_r = (D1D2 / D1R2) - 1
+  else if (estimator .eq. 'LS') then
+    xi_r = (D1D2 - D1R2 - R1D2 + R1R2) / R1R2
   else
     write(*,*) 'Estimator for the correlation function was not recognized.'
     stop
   end if
   
   ! write output  
-  open(12, file=output_filter, status='replace', form='unformatted')
-  write(12) nc
-  write(12) delta
+  open(12, file=output_filename, status='replace', form='unformatted')
+  write(12) ng1
+  write(12) xi_r
 
   call system_clock(end)
   if (debug) then
@@ -299,4 +403,5 @@ program tophat_filter
   end if
 
   end program tophat_filter
+  
   
